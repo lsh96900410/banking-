ffa09af5a45cf02e9c72f36939ff3ef6346be8af
승현 <lsh96900410@gmail.com>
2024-07-09 00:00:31 +0900

    git commit error

--- src/main/java/src/banking/common/constant/LiteralName.java
+++ src/main/java/src/banking/common/constant/LiteralName.java
@@ -6,4 +6,6 @@ import lombok.Getter;
 public class LiteralName {
 
     public static final String MIN_AMOUNT= "계좌의 잔액은 0원 이상이어야 합니다.";
+
+    public static final String NOT_EXIST_ACCOUNT = "계좌 번호를 다시 확인하세요.";
 }
