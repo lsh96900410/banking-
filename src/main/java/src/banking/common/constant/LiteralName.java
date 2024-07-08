9d51ec681f9a8879d3de061800532db5f243e885
승현 <lsh96900410@gmail.com>
2024-07-09 00:09:40 +0900

    git commit error(2)

--- src/main/java/src/banking/common/constant/LiteralName.java
+++ src/main/java/src/banking/common/constant/LiteralName.java
@@ -8,4 +8,6 @@ public class LiteralName {
     public static final String MIN_AMOUNT= "계좌의 잔액은 0원 이상이어야 합니다.";
 
     public static final String NOT_EXIST_ACCOUNT = "계좌 번호를 다시 확인하세요.";
+
+    //ab62f3e ae89a9e dc22f55 a09c5fa c1a0325
 }
